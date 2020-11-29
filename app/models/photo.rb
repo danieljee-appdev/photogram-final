ed78b3af9ccde8cec68d0c1738dfecfaf46bc941
danieljee0128 <daniel.jee@chicagobooth.edu>
2020-11-29 17:39:04 +0000

    Before Like button

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -16,4 +16,14 @@ class Photo < ApplicationRecord
   has_many(:likes, { :class_name => "Like", :foreign_key => "photo_id", :dependent => :destroy })
   belongs_to(:owner, { :required => false, :class_name => "User", :foreign_key => "owner_id" })
   has_many(:fans, { :through => :likes, :source => :fan })
+
+    def poster
+    my_owner_id = self.owner_id
+
+    matching_users = User.where({ :id => my_owner_id })
+
+    the_user = matching_users.at(0)
+
+    return the_user
+  end
 end
